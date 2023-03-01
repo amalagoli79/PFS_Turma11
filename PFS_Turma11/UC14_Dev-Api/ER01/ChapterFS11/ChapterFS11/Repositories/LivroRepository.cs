b16df9debeee56f16fcfdd15d7ee79fd8990d49a
amalagoli79 <adriano.g.malagoli@gmail.com>
2023-03-01 15:12:33 -0300

    UC14_Des.Api - ER03 - Turma11 - v2

---
+++ PFS_Turma11/UC14_Dev-Api/ER01/ChapterFS11/ChapterFS11/Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChapterFS11.Contexts;
+using ChapterFS11.Models;
+
+namespace ChapterFS11.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+    }
+}
