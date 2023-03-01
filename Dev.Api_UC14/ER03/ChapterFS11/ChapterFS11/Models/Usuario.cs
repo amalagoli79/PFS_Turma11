7e3062d4488830f4f49fab38b202b284e4eda654
amalagoli79 <adriano.g.malagoli@gmail.com>
2023-03-01 15:06:14 -0300

    UC14_Des.Api - ER03 - Turma11

---
+++ Dev.Api_UC14/ER03/ChapterFS11/ChapterFS11/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace ChapterFS11.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Senha { get; set; }
+
+        public string? Tipo { get; set; }
+
+    }
+}
---
+++ Dev.Api_UC14/ER03/ChapterFS11/ChapterFS11/Repositories/LivroRepository.cs
@@ -0,0 +1,54 @@
+﻿using ChapterFS11.Contexts;
+using ChapterFS11.Models;
+
+namespace ChapterFS11.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastrar(Livro l)
+        {
+            _context.Livros.Add(l);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Livro l = _context.Livros.Find(id);
+            _context.Livros.Remove(l);
+            _context.SaveChanges();
+
+        }
+
+        public void Alterar(int id, Livro l)
+        {
+            Livro livrobuscado = _context.Livros.Find(id);  
+
+            if (livrobuscado != null)
+            {
+                livrobuscado.Titulo = l.Titulo;
+                livrobuscado.QuantidadePaginas = l.QuantidadePaginas;
+                livrobuscado.Disponivel = l.Disponivel;
+
+                _context.Livros.Update(livrobuscado);
+                _context.SaveChanges();
+ 
+            }
+        }
+    }
+}
